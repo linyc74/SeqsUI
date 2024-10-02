ffb0fc417c1b1ce11f5fc90e757d86cb446d030f
linyc74 <ylin@nycu.edu.tw>
2024-10-02 11:22:21 +0800

    version 1.2.0

--- src/__init__.py
+++ src/__init__.py
@@ -5,7 +5,7 @@ from .model import Model
 from .controller import Controller
 
 
-VERSION = 'v1.2.0-beta'
+VERSION = 'v1.2.0'
 STARTING_MESSAGE = f'''\
 SeqsUI {VERSION}
 College of Dentistry, National Yang Ming Chiao Tung University (NYCU), Taiwan
